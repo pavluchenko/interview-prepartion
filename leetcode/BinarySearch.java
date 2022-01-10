46493c7705c830274ca64dd54b836282a9a3156b
Olga Pavluchenko <olga@pavluchenko.com>
2022-01-10 12:34:27 +0300

    Binary search examples

---
+++ leetcode/BinarySearch.java
@@ -0,0 +1,19 @@
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length -1;
+        int result = -1;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return result;
+    }
+}
